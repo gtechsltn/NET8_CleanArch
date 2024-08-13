95db568c89de3953fc5c8ae2888031424e427b6b
Nguyen Viet Manh <manh.nguyen@thirdsight.net>
2024-08-13 18:02:56 +0800

    Clean Arch

---
+++ CleanArch.Api/Program.cs
@@ -0,0 +1,62 @@
+using CleanArch.Infrastructure;
+using log4net.Config;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configure Log4net.
+XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+//Injecting services.
+builder.Services.RegisterServices();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Description = "api key.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "basic"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "basic"
+                },
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ CleanArch.Test/Helper/TestConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CleanArch.Test.Helper
+{
+    public static class TestConstants
+    {
+        public static SqlException GetSqlException()
+        {
+            var sqlException = System.Runtime.CompilerServices.RuntimeHelpers.GetUninitializedObject(typeof(SqlException)) as SqlException;
+
+            return sqlException;
+        }
+
+        public static Exception GetGeneralException()
+        {
+            return new Exception("Test Exception");
+        }
+
+        public static class ContactTest
+        {
+            public static string FirstName = "Sandeep";
+            public static string LastName = "Kumar";
+            public static string Email = "stest@gmail.com";
+            public static string PhoneNumber = "9845623117";
+            public static string NewEmail = "stest01@gmail.com";
+        }
+    }
+}
